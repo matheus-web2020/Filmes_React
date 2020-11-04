1768ba2d72b9a8ade5e9870c863dc131ff5dd447
Matheus <matheus.fusca.lima@outlook.com>
2020-11-04 09:30:13 -0300

    Finalizado

--- src/pages/filmes/index.jsx
+++ src/pages/filmes/index.jsx
@@ -23,22 +23,30 @@ class Filmes extends Component{
     }
 
     listar(){
+
         fetch(this.state.url,{
             method : 'GET'
-       })
-     
-       .then(response => response.json())
-       .then(dados => {
-           this.setState({fimes : dados})
-         console.log('filmes state' + this.state.filmes);
-       })
-       .catch(err => console.error(err));
+
+          })
+          .then( response => response.json()) 
+          .then( dados =>{
+
+            this.setState({
+
+                filmes : dados,id : '',
+                id : '',
+                nome: '',
+                categoria: '',
+                anoLancamento: ''
+            });
+
+
+          })
+          .catch(err => console.error(err));
     }
 
     remover(event){
         event.preventDefault();
-
-        console.log(event.target.value);
 
         fetch(this.state.url + '/' + event.target.value, {
             method : 'DELETE'
@@ -58,8 +66,6 @@ class Filmes extends Component{
     editar(event){
         event.preventDefault();
 
-        console.log(event.target.value);
-
         fetch(this.state.url + '/' + event.target.value, {
             method : 'PUT'
           })
@@ -77,12 +83,55 @@ class Filmes extends Component{
           this.listar();
     }
 
+    salvar(event){
+        event.preventDefault();
+
+       const filme ={
+
+          nome : this.state.nome, 
+          categoria : this.state.categoria, 
+          anoLancamento : this.state.anoLancamento
+
+       }
+
+       let filmeId = this.state.id;
+       //Condição, se for válido ""? é POST, se não for válido : é PUT
+       let method = (filmeId === ""? 'POST' : 'PUT');
+       //Condição, se for válido ""? é url, se não for válido : é url + '/' + id
+       let urlRequest = (filmeId === ""? this.state.url : this.state.url + '/' + filmeId);
+
+       fetch(urlRequest, {
+         method : method,
+         body : JSON.stringify(filme),
+         headers : {
+           'content-type' : 'application/json' 
+         }
+       })
+       .then(response => response.json)
+         .then(dado => {
+           console.log('O filme foi Salvo')
+
+           this.listar();
+         })
+       .catch(err => console.error(err));  
+      
+
+    }
+
+    limparCampos(event){
+        this.setState({
+            id : '',
+            nome : '',
+            categoria : '',
+            anoLancamento : ''
+        })
+    }
+
     setNome(event){
         event.preventDefault();
 
         this.setState({nome : event.target.value});
 
-        console.log(event.target.value);
     }
 
     render(){
@@ -92,21 +141,21 @@ class Filmes extends Component{
                 <Jumbotron titulo = "Filmes" descricao = "Gerencie seus Filmes"/>
                 <div className="container">
         <div className="bd-example" >
-        <form id="formFilme">
+        <form id="formFilme" onSubmit ={this.salvar.bind(this)}>
             <div className="form-group">
               <label htmlFor="nome">Nome</label>
               <input type="text" className="form-control" id="nome" value={this.state.nome} onChange={this.setNome.bind(this)} aria-describedby="nome" placeholder="Informe o Nome"/>
             </div>
             <div className="form-group">
               <label htmlFor="categoria">Categoria</label>
-                <input type="text" className="form-control" id="categoria" value={this.state.categoria} placeholder="Informe a Categoria"/>
+                <input type="text" className="form-control" id="categoria" value={this.state.categoria} onChange={event => this.setState({categoria : event.target.value})} placeholder="Informe a Categoria"/>
             </div>
             <div className="form-group">
                 <label htmlFor="ano">Ano de Lançamento</label>
-                <input type="text" className="form-control small" id="anoLancamento" value={this.state.anoLancamento} placeholder="Informe o Ano de Lançamento"/>
+                <input type="text" className="form-control small" id="anoLancamento" value={this.state.anoLancamento} onChange={event => this.setState({anoLancamento : event.target.value})} placeholder="Informe o Ano de Lançamento"/>
               </div>
-              <button type="button"  className="btn btn-secondary">Cancelar</button>
-            <button type="button"  className="btn btn-success">Salvar</button>
+              <button type="button"  className="btn btn-secondary" onClick={this.limparCampos.bind(this)}>Cancelar</button>
+            <button type="submit"  className="btn btn-success">Salvar</button>
         </form>
 
         <table className="table" style={{margintop: '40px'}}>
@@ -117,7 +166,7 @@ class Filmes extends Component{
                 <th scope="col">Categoria</th>
                 <th scope="col">Ano Lançamento</th>
                 <th scope="col">Ações</th>
-                <th scope="col"><button type="reset" className="btn btn-primary">Novo Filme</button></th>
+                <th scope="col"><button type="reset" className="btn btn-primary" onClick={this.limparCampos.bind(this)}>Novo Filme</button></th>
               </tr>
             </thead>
             <tbody id="tabela-lista-corpo">
@@ -131,7 +180,7 @@ class Filmes extends Component{
                             <td>{item.anoLancamento}</td>
                             <td>
                                 <button type = "button" className = "btn btn-danger" value={item.id} onClick={this.remover.bind(this)}> Remover </button>
-                                <button type = "button" className = "btn btn-warning" value={item.id} onClick={this.editar.bind(this)} >Editar </button>  
+                                <button type = "button" className = "btn btn-warning" value={item.id} onClick={this.editar.bind(this)} style={{marginLeft: '20px'}} >Editar </button>  
                             </td>
                         </tr>
                         )
